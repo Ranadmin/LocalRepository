00627cacc077687efdbf4a820aa693cd65f6996b
ran <ran@bjpowernode.com>
2019-07-23 23:36:54 +0800

    初次上传

---
+++ src/main/java/com/bjpowernode/Test.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+/**
+ * Package:com.bjpowernode
+ *
+ * @author:ranchaolin
+ * @date:2019/7/23 下午 11:35
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
